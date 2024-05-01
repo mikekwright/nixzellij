233551f909b9ed3a575d6640977e65ef3c0c9507
Michael Wright <michael@level.co>
2024-05-01 09:16:39 -0600

    Start of repo for kicking off zellij remotely

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        zellij = pkgs.zellij;
+      in {
+        packages = {
+          # Lets you run `nix run .` to start nixvim
+          default = zellij;
+        };
+      };
+    };
+}
